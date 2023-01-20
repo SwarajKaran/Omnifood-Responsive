c4a4fb33a3ad92a48b54a69dc7cc878e785f59c2
SwarajKaran <swarajkaran.raj@gmail.com>
2023-01-20 12:20:48 +0530

    added js logic to set current year in footer and make mob nav clickable

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// const h1 = document.querySelector(".header-primary");
+// console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = "Swaraj";
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+// Set current year in footer
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
